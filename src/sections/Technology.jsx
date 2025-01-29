06174ce2454a03b6ad4870fe77b0291a11485eb9
Marcel <marcel.pikul@gmail.com>
2025-01-29 18:05:21 +0100

    tftf

--- src/sections/Technology.jsx
+++ src/sections/Technology.jsx
@@ -1,7 +1,6 @@
 const Technology = ({tech}) => {
     return ( 
         <div className="section-technology border">
-            <h1>Technologia</h1>
             <div>
                 {tech.map((item,index) => (
                     <a href={item.link} key={index} target="_blanc" className="technology_item">
